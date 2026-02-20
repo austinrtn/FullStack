a5927d264b4a0e97c049aeada5cf7fbf2a5fd882
Austin Kolongowski <kolowemployment@gmail.com>
2026-02-19 22:18:01 -0500

    Added photo donwloading

--- app.go
+++ app.go
@@ -3,9 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"encoding/json"
+	//"strconv"
+	"os"
 )
 
 const PORT = ":3000"
+
+type Data struct {
+	FileName string `json:"name"`
+	FileBytes []byte `json:"file"`
+}
 
 func main() {
 	// Create a static file server using http.FileServer
@@ -27,5 +35,24 @@ func main() {
 }
 
 func helloWorld(res http.ResponseWriter, req *http.Request) {
-	log.Println("Hello world!")
+	// Create data instance to save JSON to
+	var data Data
+	var err error
+	// Decode json from req.BODY
+	err = json.NewDecoder(req.Body).Decode(&data)
+	// Check if JSON parsed
+	if err != nil {
+		http.Error(res, "Error parsing JSON", http.StatusBadRequest)
+		return
+	}
+	defer req.Body.Close()
+	
+	// Write bytes to file with permisions
+	err = os.WriteFile(data.FileName, data.FileBytes, 0644)
+	if err != nil {
+		http.Error(res, "Error writing file", http.StatusBadRequest)
+		return
+	} else {
+		log.Print("File Downloaded!")
+	}
 }
--- http/src/src.js
+++ http/src/src.js
@@ -1,7 +1,29 @@
 function handleClick() {
-	fetch("/helloWorld", {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json'},
-		body: JSON.stringify({'val': 123})
-	}).then(res => res.text());
+	const file = document.getElementById("file_btn").files[0];
+	const file_name = file.name;
+	const file_ext = file_name.split(".").pop();
+
+	if (file_ext != "jpeg" && file_ext != "png") {
+		alert("Invalid file type.  Must be .png or .jpeg");
+		return;
+	}
+
+	// Create file reader 
+	const reader = new FileReader();
+	// e is the event fired by the FileReader
+	// e.target is the file reader itself
+	// .result is where file contents are stored 
+	reader.onload = (e) => {
+		// File contents need to be base64 encoded for GO JSON parse
+		const base64 = e.target.result.split(',')[1];
+		fetch("/helloWorld", {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				"name": file_name,
+				"file": base64,
+			}),
+		}).then(res => res.blob());
+	}
+	reader.readAsDataURL(file);
 }
